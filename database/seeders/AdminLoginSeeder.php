2a823f3026e0bb83b5ef0630ffe74dd9485274b4
tunguyen196 <80661913+tunguyen196@users.noreply.github.com>
2021-12-14 17:01:46 +0700

    fix layout

--- database/seeders/AdminLoginSeeder.php
+++ database/seeders/AdminLoginSeeder.php
@@ -23,5 +23,14 @@ class AdminLoginSeeder extends Seeder
                 'password' => bcrypt('11111111'),
             ],
         );
+        DB::table('users')->insert(
+            [
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'role' => 0,
+                'level' => 1,
+                'password' => bcrypt('12345678'),
+            ],
+        );
     }
 }
