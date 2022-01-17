69cf971eaf1464ddeaf37d515a18705b38f49653
daudangtung1 <dangtung.dau.bka@gmail.com>
2022-01-18 05:13:38 +0700

    fix undone

--- public/js/user-overtime-fix.js
+++ public/js/user-overtime-fix.js
@@ -4,18 +4,34 @@ $(document).ready(function () {
     });
     $("#checkout_form").DateTimePicker();
 
+    $("#checkin_date").DateTimePicker({
+        dateTimeFormat: "yyyy-MM-dd",
+    })
 
     $("#checkout").change(function () {
-        var checkin = new Date($("#checkin").val());
+
+        var checkinDate = $("#date").val();
+        var checkin_y = checkinDate.getFullYear();
+        var checkin_m = checkinDate.getMonth();
+        var checkin_d = checkinDate.getDate();
+
+        var checkinTime = $("#checkin").val();
+        var checkin_h = checkinTime.getHours();
+        var checkin_m = checkinTime.getMinutes();
+
+        var checkin = new Date(checkin_y, checkin_m, checkin_d, checkin_h, checkin_m);
+        console.log(checkin);
+
+
         var checkout = new Date($("#checkout").val());
 
-        var checkin_day=checkin.getDate();
-        var checkin_month=checkin.getMonth();
-        var checkin_year=checkin.getFullYear();
+        var checkin_day = checkin.getDate();
+        var checkin_month = checkin.getMonth();
+        var checkin_year = checkin.getFullYear();
 
-        var checkout_day=checkin.getDate();
-        var checkout_month=checkin.getMonth();
-        var checkout_year=checkin.getFullYear();
+        var checkout_day = checkin.getDate();
+        var checkout_month = checkin.getMonth();
+        var checkout_year = checkin.getFullYear();
 
         var checkinStart = new Date(checkin_year, checkin_month, checkin_day, 17, 59, 0, 0);
         var checkinEnd = new Date(checkin_year, checkin_month, checkin_day, 6, 59, 0, 0);
